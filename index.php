1af1d9ec7df1e73d9e01d8cd1bd9391125f06c1e
muriLLLo7 <164691968+muriLLLo7@users.noreply.github.com>
2024-11-29 16:03:43 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,47 @@
+<h2>Receita
+    <?php
+    echo "Bolo de fubá";
+    ?>
+    </h2>
+    <table>
+        <thead>
+            <tr>
+                <th>ingrediente</th>
+                <th>quantidade</th>
+</tr>
+</thead>
+<tbody>
+    <tr>
+        <td>ovo</td>
+        <td>3</td>
+</tr>
+<tr>
+    <td>fubá</td>
+    <td>2 xícaras (chá)</td>
+</tr>
+<tr>
+    <td>óleo</td>
+    <td>1/2 copo americano</td>
+</tr>
+<tr>
+    <td>fermento</td>
+    <td>1 colher (sopa)</td>
+</tr>
+<tr>
+    <td>açúcar</td>
+    <td>2 xícaras (chá)</td>
+</tr>
+<tr>
+    <td>farinha de trigo</td>
+    <td>3 colheres (sopa)</td>
+</tr>
+<tr>
+    <td>leite</td>
+    <td>1 copo americano</td>
+</tr>
+</table>
+<h1>Nota</h1>
+<script>
+    var h1 = document.querySelector("h1");
+    h1.innerHTML = "Nota:10";
+    </script>
